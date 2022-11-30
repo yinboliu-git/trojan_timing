82b306375af89c2aaa641eac500e2607a043b62c
Yinbo Liu <92346875+yinboliu-git@users.noreply.github.com>
2022-11-30 13:18:25 +0800

    Update trojan.py

--- task/trojan.py
+++ task/trojan.py
@@ -16,7 +16,7 @@ while True:
     M = now.strftime("%M")
     D = now.strftime("%d")
     print(f'当前时间:{D}日{H}点{M}分')
-    if H == '09':
+    if H == '09':    ########### 修改此行数字09(代表09点)更改时间 00 01 02 ... 23 ########### 
         cmd_0 = 'bash ./trojan.sh'
         a=os.system(cmd_0)
         print('trojan执行成功...')
